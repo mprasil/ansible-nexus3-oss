9b6da78ff629dc564b6234d181a2c59a5ec63fd2
Tom von Schwerdtner <tvon@covermymeds.com>
2017-01-18 12:03:55 -0500

    Enable setting auth scheme

--- files/groovy/setup_ldap.groovy
+++ files/groovy/setup_ldap.groovy
@@ -26,7 +26,13 @@ ldapConfig.setName(parsed_args.name)
 // Connection
 connection = new Connection()
 connection.setHost(new Connection.Host(Connection.Protocol.valueOf(parsed_args.protocol), parsed_args.hostname, Integer.valueOf(parsed_args.port)))
-connection.setAuthScheme("none")
+if (parsed_args.auth_scheme) {
+  connection.setAuthScheme(parsed_args.auth_scheme)
+  connection.setSystemUsername(parsed_args.auth_user)
+  connection.setSystemPassword(parsed_args.auth_password)
+} else {
+  connection.setAuthScheme("none")
+}
 connection.setSearchBase(parsed_args.search_base)
 connection.setConnectionTimeout(30)
 connection.setConnectionRetryDelay(300)
